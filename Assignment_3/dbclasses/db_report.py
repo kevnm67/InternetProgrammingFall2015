5651be716ce4a7deb1f2e0c3c22cf0b6b6a96ab7
Kevin Morton <kevnm67@gmail.com>
2015-09-23 18:11:15 -0400

    refactoring

--- Assignment_3/dbclasses/db_report.py
+++ Assignment_3/dbclasses/db_report.py
@@ -3,16 +3,21 @@ __author__ = 'KevinMortonMacPro'
 import db_access
 import db_utility
 
-# ------------------------------------| Formatting functions |----------------------------------------------
+# ------------------------------------| Formatting |----------------------------------------------
+areas = db_access.get_all_areas()
+template = "{:>5}   {:20} {:<20} {:.5}  {:<20}  "
+row = template.format("ID", "Area Name", "# of locations", "Avg Value", "Category")
+print(row)
+
+# ------------------------------------| Formatting functions |------------------------------------
 def getCategoryString(aList):
+    # Pass a list of categories to get a formatted comma separated string    
     categoryString = ''
     theCategory = []
-    countNum = 0
-
+    
     try:
         for i in aList:
            theCategory.append(i['name'])
-     
         countString = len(area_category)
         
         for i in theCategory:
@@ -20,33 +25,21 @@ def getCategoryString(aList):
             if countString > 1:
                  categoryString += ', '
                  countString += -1
-            
     except TypeError:
         categoryString = 'None'
-        
     return categoryString
-
-# ------------------------------------| Formatting |----------------------------------------------
-areas = db_access.get_all_areas()
-template = "{:>5}   {:20} {:<20} {:.5}  {:<20}  "
-row = template.format("ID", "Area Name", "# of locations", "Avg Value", "Category")
-print(row)
 
 # ------------------------------------| Pretty Print |----------------------------------------------
 for area in areas:  
-    
     area_id = area['area_id']
     area_location = db_access.get_locations_for_area(area_id)
     area_category = db_access.get_categories_for_area(area_id)
     avgMeasurementString = str( db_utility.get_average_measurements_for_area(area_id))
     
     if not db_utility.get_average_measurements_for_area(area_id):
-        avgMeasurementString = '----------'
+        avgMeasurementString = '--------'
         
-# -------| Final output |----------------------------------------------
+    # --| Final output | --
     row = template.format(area_id, area['name'], len(area_location), avgMeasurementString,getCategoryString(area_category))
     
     print(row)
-    
-    
-    
