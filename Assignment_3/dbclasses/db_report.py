0e31e55fbc9324a8d5a338a4f749165d435168b4
Kevin Morton <kevnm67@gmail.com>
2015-09-23 17:45:37 -0400

    Category function FINALLY working with the test file

--- Assignment_3/dbclasses/db_report.py
+++ Assignment_3/dbclasses/db_report.py
@@ -4,28 +4,37 @@ import db_access
 import db_utility
 
 areas = db_access.get_all_areas()
+# ------------------------------------| Formatting |----------------------------------------------
 template = "{:>5}   {:20} {:<20} {:.5}  {:<20}  "
-
 row = template.format("ID", "Area Name", "# of locations", "Avg Value", "Category")
 print(row)
 
-for area in areas:    
+# ------------------------------------| Pretty Print |----------------------------------------------
+for area in areas:  
     area_id = area['area_id']
     area_location = db_access.get_locations_for_area(area_id)
     area_category = db_access.get_categories_for_area(area_id)
+    
     theCategory = ''
-   
-    if not area_category:
-        theCategory = 'none'
-    else:
-        theCategory = area_category[0]['name']
-  
+    theS = ''
+    countNum = 0
+    
+    try:
+        print('area count\t',len(area_location))
+        for i in area_category:
+           theCategory += i['name']
+    except TypeError:
+        theCategory = 'None'
+
     avgMeasurementString = str( db_utility.get_average_measurements_for_area(area_id))
+    
     if not db_utility.get_average_measurements_for_area(area_id):
         avgMeasurementString = '----------'
         
-  
-    row = template.format(area_id, area['name'], len(areas), avgMeasurementString,theCategory)
+        
+# -------| Final output |----------------------------------------------
+    row = template.format(area_id, area['name'], len(area_location), avgMeasurementString,theCategory)
     print(row)
     
-    	
+    
+    
